b0394dde8a763410b86b5271882e2852e5acd768
mozamimy <alice@mozami.me>
2018-05-19 16:36:11 +0900

    Implement `iokuji` command

--- lib/bts_bot/cli.rb
+++ lib/bts_bot/cli.rb
@@ -11,24 +11,25 @@ module BtsBot
     def initialize(argv)
       @argv = argv.dup
       parser.parse!(@argv)
-    end
 
-    def run
-      bot = Discordrb::Commands::CommandBot.new(
+      @bot = Discordrb::Commands::CommandBot.new(
         client_id: ENV.fetch('CLIENT_ID'),
         token: ENV.fetch('SECRET_TOKEN'),
         prefix: '/',
       )
+      Iokuji.new(@iokuji_file_path).set_callback(@bot)
 
-      bot.command :echo do |e, str|
+      @bot.command :echo do |e, str|
         str
       end
 
-      bot.command :usagi do |e, str|
+      @bot.command :usagi do |e, str|
         'かわいい'
       end
+    end
 
-      bot.run
+    def run
+      @bot.run
     end
 
     private
@@ -37,6 +38,7 @@ module BtsBot
       @parser ||= OptionParser.new do |opts|
         opts.banner = 'bts_bot'
         opts.version = BtsBot::VERSION
+        opts.on('-i', '--iokuji=FILE', 'The iokuji spec file path') { |i| @iokuji_file_path = i }
       end
     end
   end
