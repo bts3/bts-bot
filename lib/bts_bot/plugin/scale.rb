ce36ebb368b6a367abe739db8116d7a511064f80
Carl Ehrenstrahle <barriz0r@gmail.com>
2018-05-26 12:23:43 +0200

    Add scale command.

---
+++ lib/bts_bot/plugin/scale.rb
@@ -0,0 +1,36 @@
+module BtsBot
+  module Plugin
+    class Scale
+      attr_reader :command, :description
+
+      def initialize
+        @command = :scale
+        @description = 'Randomize a scale for the requested input'
+      end
+
+      def set_callback(bot)
+        bot.command(@command, description: @description) do |e, str|
+          invoke(str)
+        end
+      end
+
+      def invoke(str)
+        score = rand(100)
+
+        if score == 0
+          bar = "[__________]"
+        elsif score == 100
+          bar = "[==========]"
+        else
+          numthick = score / 10
+          bar = "["
+          (numthick - 1).times { bar = bar + "=" }
+          bar = bar + ">"
+          (10 - numthick).times { bar = bar + "_"}
+          bar = bar + "]"
+        end
+        "Your score on the #{str} scale is: #{bar} #{score}%"
+      end
+    end
+  end
+end
